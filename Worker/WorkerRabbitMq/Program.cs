e587d876e9957cb13685db5510067b1062e575c0
allansud <freitasallan@gmail.com>
2021-04-21 17:18:18 -0300

    Added easyNetQ

--- Worker/WorkerRabbitMq/Program.cs
+++ Worker/WorkerRabbitMq/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.IO;
 
 namespace WorkerRabbitMq
 {
@@ -12,6 +14,13 @@ namespace WorkerRabbitMq
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", true, true)
+                    .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                    .AddEnvironmentVariables();
+                })
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<Worker>();
